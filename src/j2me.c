bcc1d7ea68f0764b726023431cd8cee2b9acbf0c
wychenjiechen <wychenjiechen@jd.com>
2017-08-01 11:24:03 +0800

    j2me cldc

---
+++ src/j2me.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : j2me.c
+ Author      : wychenjiechen
+ Version     :
+ Copyright   : j2me
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("cldc"); /* prints cldc */
+	return EXIT_SUCCESS;
+}
